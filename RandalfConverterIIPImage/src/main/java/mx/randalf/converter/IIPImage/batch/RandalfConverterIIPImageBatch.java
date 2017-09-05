f2f37c6b59d93cb03021869062c43ad85b3a0c00
Massimiliano Randazzo <massimiliano.randazzo@gmail.com>
2017-09-05 15:44:02 +0200

    Inplementazione coneverisone per il protocolo IIIF

--- RandalfConverterIIPImage/src/main/java/mx/randalf/converter/IIPImage/batch/RandalfConverterIIPImageBatch.java
+++ RandalfConverterIIPImage/src/main/java/mx/randalf/converter/IIPImage/batch/RandalfConverterIIPImageBatch.java
@@ -2,6 +2,7 @@ package mx.randalf.converter.IIPImage.batch;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.Arrays;
 
 import org.apache.log4j.Logger;
 import org.im4java.process.ProcessStarter;
@@ -14,6 +15,8 @@ import mx.randalf.converter.IIPImage.RandalfConverterIIPImage;
 import mx.randalf.converter.IIPImage.exception.RandalfConverterIIPImageException;
 import mx.randalf.interfacException.exception.PubblicaException;
 import mx.randalf.mag.MagXsd;
+import mx.randalf.tools.Utils;
+import mx.randalf.tools.exception.UtilException;
 import mx.randalf.xsd.exception.XsdException;
 
 public class RandalfConverterIIPImageBatch {
@@ -21,22 +24,39 @@ public class RandalfConverterIIPImageBatch {
 	private Logger log = Logger.getLogger(RandalfConverterIIPImageBatch.class);
 
 	private String pathImageMagick = null;
+
+	private String ext = null;
+
+	private String level = null;
 	
-	public RandalfConverterIIPImageBatch(String pathImageMagick) {
+	public RandalfConverterIIPImageBatch(String pathImageMagick, String tipologiaMateriale) {
 		this.pathImageMagick = pathImageMagick;
+
+		if (tipologiaMateriale.equals("IIPSRV")){
+			ext = "iip";
+			level = "6";
+		} else {
+			ext = "iiif";
+			level = "7";
+		}
 		ProcessStarter.setGlobalSearchPath(pathImageMagick);
 	}
 
 	public static void main(String[] args) {
 		RandalfConverterIIPImageBatch batch = null;
 
-		if (args.length==2){
-			batch = new RandalfConverterIIPImageBatch(args[0]);
-			batch.scan(args[1]);
+		if (args.length==3){
+			if (args[2].equals("IIPSRV") || args[2].equals("IIIF")){
+				batch = new RandalfConverterIIPImageBatch(args[0], args[2]);
+				batch.scan(args[1]);
+			} else {
+				System.err.println("La titppolgia di materiale non e' corretta deve essere IIPSRV oppure IIIF");
+			}
 		} else {
 			System.out.println("E' necessaio indicare i seguenti parametri:");
 			System.out.println("1) Path imagemagick");
 			System.out.println("2) Path da analizzare");
+			System.out.println("2) Tipologia di materiale (IIPSRV/IIIF)");
 		}
 	}
 
@@ -68,7 +88,7 @@ public class RandalfConverterIIPImageBatch {
 					if (pathname.isDirectory()){
 						result = true;
 					} else if (pathname.getName().toLowerCase().endsWith(".xml")){
-						fIIP = new File(pathname.getAbsolutePath()+".iip");
+						fIIP = new File(pathname.getAbsolutePath()+"."+ext);
 						if (!fIIP.exists()){
 							result = true;
 						}
@@ -77,6 +97,7 @@ public class RandalfConverterIIPImageBatch {
 				return result;
 			}
 		});
+		Arrays.sort(fl);
 		for (File f: fl){
 			if (f.isDirectory()){
 				scan(f);
@@ -93,6 +114,9 @@ public class RandalfConverterIIPImageBatch {
 		File fImgOutput = null;
 		File fCert = null;
 		File fIip = null;
+		File fSolr = null;
+		File fElabOK = null;
+		File fElabKO = null;
 		Link file = null;
 
 		try {
@@ -105,7 +129,7 @@ public class RandalfConverterIIPImageBatch {
 					if ((x%10)==0){
 						System.out.println("img "+x+"/"+mag.getImg().size());
 					}
-					if (!checkUsage(mag.getImg().get(x), "6")){
+					if (!checkUsage(mag.getImg().get(x), level)){
 						file = getFile(mag.getImg().get(x), "1");
 						if (file != null){
 							fImgInput = null;
@@ -119,7 +143,7 @@ public class RandalfConverterIIPImageBatch {
 															file.getHref())));
 							if (fImgInput.exists()){
 								fImgOutput = new File(fImgInput.getParentFile().getParentFile().getAbsolutePath()+
-										File.separator+"IIP"+File.separator+
+										File.separator+ext.toUpperCase()+File.separator+
 										fImgInput.getName());
 								convert(fImgInput, fImgOutput);
 								mag.
@@ -134,7 +158,7 @@ public class RandalfConverterIIPImageBatch {
 										mag.getImg().get(x), 
 										fi.getParentFile().getAbsolutePath(), 
 										true, 
-										new String[] {"1","6"});
+										new String[] {"1",level});
 							} else {
 								log.error("Il file ["+fImgInput.getAbsolutePath()+"] non esiste");
 							}
@@ -142,11 +166,37 @@ public class RandalfConverterIIPImageBatch {
 					}
 				}
 				System.out.println("Scrivo il file : "+fi.getAbsolutePath());
+				fCert = new File(fi.getAbsolutePath()+".cert");
+				if (fCert.exists()){
+					if (!fCert.delete()){
+						throw new RandalfConverterIIPImageException("Problemi nella cancellazione del file ["+fCert.getAbsolutePath()+"]");
+					}
+				}
 				if (magXsd.write(mag, fi)){
 					fCert = new File(fi.getAbsolutePath()+".cert");
 					if (fCert.exists()){
-						fIip = new File(fi.getAbsolutePath()+".iip");
-						if (!fCert.renameTo(fIip)){
+						fIip = new File(fi.getAbsolutePath()+"."+ext);
+						if (Utils.copyFileValidate(fCert.getAbsolutePath(), fIip.getAbsolutePath())){
+							fSolr = new File(fi.getAbsolutePath()+".solr");
+							if (fSolr.exists()){
+								if (fSolr.delete()){
+									fElabOK = new File(fi.getAbsolutePath()+".elabOK");
+									if (fElabOK.exists()){
+										if (!fElabOK.delete()){
+											log.error("Problemi nella cancellazione del file ["+fElabOK.getAbsolutePath()+"]");
+										}
+									}
+									fElabKO = new File(fi.getAbsolutePath()+".elabKO");
+									if (fElabKO.exists()){
+										if (!fElabKO.delete()){
+											log.error("Problemi nella cancellazione del file ["+fElabKO.getAbsolutePath()+"]");
+										}
+									}
+								} else {
+									log.error("Problemi nella cancellazione del file ["+fSolr.getAbsolutePath()+"]");
+								}
+							}
+						} else {
 							log.error("Problemi nella rinomina del file ["+
 									fCert.getAbsolutePath()+
 									"] in ["+
@@ -160,6 +210,8 @@ public class RandalfConverterIIPImageBatch {
 			log.error(e.getMessage(), e);
 		} catch (PubblicaException e) {
 			log.error(e.getMessage(), e);
+		} catch (UtilException e) {
+			log.error(e.getMessage(), e);
 		}
 	}
 
@@ -171,7 +223,7 @@ public class RandalfConverterIIPImageBatch {
 		file = new Link();
 		file.setHref(hrefFile);
 		altimg.setFile(file);
-		altimg.getUsage().add("6");
+		altimg.getUsage().add(level);
 		return altimg;
 	}
 
